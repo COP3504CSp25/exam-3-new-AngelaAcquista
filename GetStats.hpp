a2a7309031de0b035add4012d3d9ca5eaa7ea3c0
Angela Acquista <157067144+AngelaAcquista@users.noreply.github.com>
2025-04-17 20:47:13 +0000

    getstats

--- GetStats.hpp
+++ GetStats.hpp
@@ -15,6 +15,32 @@ public:
 
     int* getStats() const{
         //write your function here!
+        int* arr = new int[3];
+        arr[0] = 0;
+        arr[1] = 0;
+        arr[2] = 0;
+        if(head == nullptr){
+            int* ptr = &arr[0];
+            return ptr;
+        }
+        Node* current = head;
+        arr[0] = current->data;
+        arr[1] = current->data;
+        int count;
+        while(current->next != nullptr){
+            if(arr[0] > current->data){
+                arr[0] = current->data;
+            }
+            if(arr[1] < current->data){
+                arr[1] = current->data;
+            }
+            arr[2] += current->data;
+            current = current->next;
+            count++;
+        }
+        arr[2] /= count;
+        int* ptr = &arr[0];
+        return ptr;
     }
     
 

e164a74179932132070b52dfe26781570a3a9d85
Angela Acquista <157067144+AngelaAcquista@users.noreply.github.com>
2025-04-17 21:51:54 +0000

    checking if this works

--- ReverseList.hpp
+++ ReverseList.hpp
@@ -21,16 +21,15 @@ public:
             return list;
         }
         Node* current = head;
+        list.addHead(current->data);
         if(current->next != nullptr){
             list.addHead(current->data);
             return list;
         }
-
         while(current->next != nullptr){
             current = current->next;
-
+            list.addHead(current->data);
         }
-
         return list;
     }
 
